5e43674d42e54b1af0195e0f630b54abe750b8c9
wxlgr <1804643502@qq.com>
2022-11-25 21:52:02 +0800

    完成验证码

--- captcha/code.php
+++ captcha/code.php
@@ -3,6 +3,6 @@ session_start();
 include './Captcha.php';
 $code = new Captcha(100,30,5);
 $codetext = $code->render();
-file_put_contents('./code.txt',$codetext);
+// file_put_contents('./code.txt',$codetext);
 
 $_SESSION["captcha"] = $codetext;
